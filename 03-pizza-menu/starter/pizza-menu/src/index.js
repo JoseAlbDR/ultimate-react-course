595b12eb5e440984403f12ede473c4c0536643a8
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-06-08 00:10:49 +0200

    Header, Menu, Footer components

--- 03-pizza-menu/starter/pizza-menu/src/index.js
+++ 03-pizza-menu/starter/pizza-menu/src/index.js
@@ -5,11 +5,31 @@ import { pizzaData } from "./data.js";
 function App() {
   return (
     <div>
-      <h1>Hello React!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently Open!</footer>
   );
 }
 
@@ -18,7 +38,7 @@ function Pizza() {
     <div>
       <img src={pizzaData[2].photoName} alt={pizzaData[2].name} />
       <h2>{pizzaData[2].name}</h2>
-      <p>{pizzaData[2].ingredients}</p>;
+      <p>{pizzaData[2].ingredients}</p>
     </div>
   );
 }
